361d02a6c3e054ee30727adbe75cd34ba92e03d6
er-karim <errakkarim@gmail.com>
2019-07-27 12:53:43 +0100

    test(factories): update buildings factory

--- spec/factories/buildings.rb
+++ spec/factories/buildings.rb
@@ -1,10 +1,19 @@
 FactoryBot.define do
   factory :building do
     reference { 1 }
-    address { "MyString" }
-    zip_code { "MyString" }
-    city { "MyString" }
-    country { "MyString" }
-    manager_name { "MyString" }
+    address { "adress" }
+    zip_code { "111111" }
+    city { "city" }
+    country { "country" }
+    manager_name { "manager name" }
+
+    trait :random_building do
+      reference { rand(2..10000) }
+      address { Faker::Address.street_address }
+      zip_code { Faker::Address.zip_code }
+      city { Faker::Address.city }
+      country { Faker::Address.country }
+      manager_name { Faker::Name.name }
+    end
   end
 end

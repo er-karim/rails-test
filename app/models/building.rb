a2a1079ea6ffa65bd92a9494ce2a62c52380f511
er-karim <errakkarim@gmail.com>
2019-07-27 12:46:53 +0100

    feat(models): add more validation tests for building and person models

--- app/models/building.rb
+++ app/models/building.rb
@@ -1,3 +1,4 @@
 class Building < ApplicationRecord
   validates :manager_name, presence: true, length: {minimum: 2}
+  validates :reference, presence: true, uniqueness: true, numericality: { only_integer: true }
 end
--- app/models/person.rb
+++ app/models/person.rb
@@ -1,4 +1,5 @@
 class Person < ApplicationRecord
-  validates :firstname, :lastname, :email, :mobile_phone_number, presence: true
+  validates :reference, :firstname, :lastname, :email, :mobile_phone_number, presence: true
   validates :email, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP }
+  validates :reference, uniqueness: true, numericality: { only_integer: true }
 end

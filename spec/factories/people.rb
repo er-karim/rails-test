22849ad6f07232b5de928e7e342c15450004f040
er-karim <errakkarim@gmail.com>
2019-07-27 16:03:14 +0100

    test(factories): update people factory

--- spec/factories/people.rb
+++ spec/factories/people.rb
@@ -2,10 +2,20 @@ FactoryBot.define do
   factory :person do
     reference { 1 }
     email { "mystring@test.dev" }
-    home_phone_number { "MyString" }
-    mobile_phone_number { "MyString" }
-    firstname { "MyString" }
-    lastname { "MyString" }
-    address { "MyString" }
+    home_phone_number { "0511111111" }
+    mobile_phone_number { "0622222222" }
+    firstname { "firstname" }
+    lastname { "lastname" }
+    address { "address" }
+
+    trait :random_person do
+      reference { rand(2..10000) }
+      email { Faker::Internet.email }
+      home_phone_number { Faker::PhoneNumber.cell_phone }
+      mobile_phone_number { Faker::PhoneNumber.phone_number }
+      firstname { Faker::Name.first_name }
+      lastname { Faker::Name.last_name }
+      address { Faker::Address.street_address }
+    end
   end
 end

bf6556c19cbe6ca23f0e72f17d4b14134fc43f6a
er-karim <errakkarim@gmail.com>
2019-08-01 01:44:29 +0100

    refactor(models): add polymorphic association to building and person model with FIELDS_TO_CHECK_BEFORE_UPDATE constant

--- app/models/building.rb
+++ app/models/building.rb
@@ -1,4 +1,8 @@
 class Building < ApplicationRecord
+  include Updatable
+
+  FIELDS_TO_CHECK_BEFORE_UPDATE = [:manager_name].freeze
+
   validates :manager_name, presence: true, length: {minimum: 2}
   validates :reference, presence: true, uniqueness: true, numericality: { only_integer: true }
 end
--- app/models/person.rb
+++ app/models/person.rb
@@ -1,4 +1,8 @@
 class Person < ApplicationRecord
+  include Updatable
+
+  FIELDS_TO_CHECK_BEFORE_UPDATE = %I(email home_phone_number mobile_phone_number address).freeze
+
   validates :reference, :firstname, :lastname, :email, :mobile_phone_number, presence: true
   validates :email, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP }
   validates :reference, uniqueness: true, numericality: { only_integer: true }
